5b9daa8f85109d8406b4be8d56de8189e2107d2f
MatiasRosales <matias.rosales97@hotmail.com>
2022-05-01 11:04:47 -0300

    ajuste address, se quitan tests de funcionamiento y conversion de logs a español

--- Memory/src/memory_main.c
+++ Memory/src/memory_main.c
@@ -2,19 +2,6 @@
 int main(void){
     init_global_variables();
     init_memory();
-    /* TEST DE FUNCIONAMIENTO DE NUEVOS METODOS
-    */
-    save_value_in_memory(0,5,1);
-    save_value_in_memory(4,6,1);
-    save_value_in_memory(8,7,1);
-    read_value_in_memory(4,1);
-    read_value_in_memory(8,1);
-    create_swap_file(1);
-    save_content_in_swap_file(0, 12,1);
-    load_file_into_memory(1,12,12);
-    read_value_in_memory(12,1);
-    read_value_in_memory(16,1);
-    read_value_in_memory(20,1);
 
     return EXIT_SUCCESS;
 }
@@ -22,27 +9,27 @@ int main(void){
 
 void init_memory(){
     uint32_t size = atoi(config_get_string_value(CONFIG,"TAM_MEMORIA"));
-    MEMORY_BLOCK = malloc(size);
+    void * MEMORY_BLOCK = malloc(size);
 }
 
 void init_global_variables(){
-    LOGGER = init_logger();
-    log_info(LOGGER,"Logger created");
-    CONFIG = init_config();
-    log_info(LOGGER,"Config created");
-    PATH_SWAP_BASE = config_get_string_value(CONFIG,"PATH_SWAP");
+    t_log* LOGGER = init_logger();
+    log_info(LOGGER,"Logger creado");
+    t_config *CONFIG = init_config();
+    log_info(LOGGER,"Config creada");
+    char* PATH_SWAP_BASE = config_get_string_value(CONFIG,"PATH_SWAP");
 }
 
-void save_value_in_memory(uint32_t direction, uint32_t value,uint32_t pid){
-    memcpy(MEMORY_BLOCK+ direction, &value, sizeof(uint32_t));
+void save_value_in_memory(uint32_t address, uint32_t value,uint32_t pid){
+    memcpy(MEMORY_BLOCK+ address, &value, sizeof(uint32_t));
 
-    log_info(LOGGER,"The value %d was saved at direction %d for PID %d",value,direction,pid);
+    log_info(LOGGER,"Se guardo el valor %d en la direccion %d para el PID %d",value,address,pid);
 }
 
-uint32_t read_value_in_memory(uint32_t direction,uint32_t pid){
+uint32_t read_value_in_memory(uint32_t address,uint32_t pid){
     uint32_t value;
-    memcpy(&value,MEMORY_BLOCK +direction,sizeof(uint32_t));
-    log_info(LOGGER,"The value %d obtained in direction %d for PID %d",value,direction,pid);
+    memcpy(&value,MEMORY_BLOCK +address,sizeof(uint32_t));
+    log_info(LOGGER,"Se obtuvo el valor %d en la direccion %d para el PID %d",value,address,pid);
     return value;
 }
 
@@ -60,7 +47,7 @@ void create_swap_file(uint32_t pid){
     char* path = generate_path_file(pid);
     SWAP_FILE = fopen(path,"w+");
     if(SWAP_FILE == NULL){
-        log_info(LOGGER,"could not create  SWAP_FILE %s",path);
+        log_info(LOGGER,"no se pudo abrir el archivo de swap %s",path);
     }
     log_info(LOGGER,"SWAP_FILE created: %d.swap", pid);
     fclose(SWAP_FILE);
@@ -74,34 +61,32 @@ char* generate_path_file(uint32_t pid){
     return path;
 }
 
-void save_content_in_swap_file(uint32_t direction,uint32_t size,uint32_t pid){
+void save_content_in_swap_file(uint32_t address,uint32_t size,uint32_t pid){
     //PRECONDICION: EL SWAP_FILE para el pid ya debe existir
     char* path = generate_path_file(pid);
     SWAP_FILE = open(path,O_CREAT|O_RDWR, 07777);
     if(SWAP_FILE == -1){
-        log_info(LOGGER,"No se pudo abrir el SWAP_FILE %s",path);
+        log_info(LOGGER,"No se pudo abrir el archivo de swap %s",path);
     }else{
         //inicio de la memoria SECONDARY con mmap
         MEMORY_BLOCK_SECONDARY = mmap(NULL, size,PROT_WRITE|PROT_READ,MAP_SHARED|MAP_FILE,SWAP_FILE, 0);
         ftruncate(SWAP_FILE, size);
-        //memset(MEMORY_BLOCK_SECONDARY, ' ', size_SWAP);
-        //msync(MEMORY_BLOCK_SECONDARY, size_MEMORIA_SECONDARY, MS_SYNC); //sincronizar
-        memcpy(MEMORY_BLOCK_SECONDARY,MEMORY_BLOCK + direction,size);
+        memcpy(MEMORY_BLOCK_SECONDARY,MEMORY_BLOCK + address,size);
         msync(MEMORY_BLOCK_SECONDARY, size, MS_SYNC);
         close(SWAP_FILE);
 
     }
 }
 
-void load_file_into_memory(uint32_t pid,uint32_t direction,uint32_t size){
+void load_file_into_memory(uint32_t pid,uint32_t address,uint32_t size){
     char* path = generate_path_file(pid);
     SWAP_FILE = open(path,O_CREAT|O_RDWR, 07777);
     if(SWAP_FILE == -1){
-        log_info(LOGGER,"Could not open SWAP_FILE %s",path);
+        log_info(LOGGER,"No se pudo abrir el archivo de swap %s",path);
     }else{
         MEMORY_BLOCK_SECONDARY = mmap(NULL, size,PROT_WRITE|PROT_READ,MAP_SHARED|MAP_FILE,SWAP_FILE, 0);
         ftruncate(SWAP_FILE, size);
-        memcpy(MEMORY_BLOCK + direction,MEMORY_BLOCK_SECONDARY,size);
+        memcpy(MEMORY_BLOCK + address,MEMORY_BLOCK_SECONDARY,size);
         msync(MEMORY_BLOCK_SECONDARY, size, MS_SYNC);
         close(SWAP_FILE);
     }
--- Memory/src/memory_main.h
+++ Memory/src/memory_main.h
@@ -15,12 +15,12 @@
 #include<fcntl.h>
 #include<unistd.h>
 //VARIABLES GLOBALES
-void* MEMORY_BLOCK;
-t_log* LOGGER;
-t_config* CONFIG;
-char* PATH_SWAP_BASE;
-FILE* SWAP_FILE;
-void* MEMORY_BLOCK_SECONDARY;
+extern void* MEMORY_BLOCK;
+extern t_log* LOGGER;
+extern t_config* CONFIG;
+extern char* PATH_SWAP_BASE;
+extern FILE* SWAP_FILE;
+extern void* MEMORY_BLOCK_SECONDARY;
 //VARIABLES GLOBALES
 
 
@@ -31,12 +31,12 @@ void init_global_variables(void);
 
 
 void init_memory(void);
-void save_value_in_memory(uint32_t direction, uint32_t value,uint32_t pid);
-uint32_t read_value_in_memory(uint32_t direction,uint32_t pid);
+void save_value_in_memory(uint32_t address, uint32_t value,uint32_t pid);
+uint32_t read_value_in_memory(uint32_t address,uint32_t pid);
 
 void create_swap_file(uint32_t pid);
 char* generate_path_file(uint32_t pid);
-void save_content_in_swap_file(uint32_t direction,uint32_t size,uint32_t pid);
-void load_file_into_memory(uint32_t pid,uint32_t direction,uint32_t size);
+void save_content_in_swap_file(uint32_t address,uint32_t size,uint32_t pid);
+void load_file_into_memory(uint32_t pid,uint32_t address,uint32_t size);
 
 #endif //MEMORY_MEMORY_MAIN_H
