8ee94d309e0d43bcf329b03f9e2cfb793f25ba31
FrancoMila <franco.milanese91@gmail.com>
2022-05-02 15:16:15 -0300

    initialization

---
+++ Kernel/src/kernel_sanitizer.c
@@ -0,0 +1,18 @@
+#include "../include/kernel_sanitizer.h"
+#include "../../Utils/include/pretty_printer.h"
+#include "../../Utils/include/serializable_objects.h"
+#include "../../Utils/include/garbage_collector.h"
+#include "../../Utils/include/configuration_manager.h"
+#include "../../Utils/include/general_logs.h"
+#include "../include/kernel_logs_manager.h"
+
+void free_system() {
+
+    free_pretty_printer();
+    free_serializable_objects();
+    free_garbage_collector();
+    free_configuration_manager();
+
+    log_successful_clean_up();
+    free_kernel_logs_manager();
+}
