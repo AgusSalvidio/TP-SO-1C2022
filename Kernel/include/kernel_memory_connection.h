096d809f1dd521755eec106ce2fcd99cbf3f80ba
FrancoMila <franco.milanese91@gmail.com>
2022-05-29 20:10:03 -0300

    added cpu connection

--- Kernel/include/kernel_memory_connection.h
+++ Kernel/include/kernel_memory_connection.h
@@ -1,7 +1,7 @@
 #ifndef KISS_KERNEL_MEMORY_CONNECTION_H
 #define KISS_KERNEL_MEMORY_CONNECTION_H
 
-#include "../../Utils/include/common_structures.h"
+#include <stdint.h>
 
 void connect_and_send_to_memory(uint32_t operation, void *structure_to_send);
 
